5a010c77b0d2eb6b2974e9b1406a53a73000204c
Arthur Woimbée <arthur.woimbee@gmail.com>
2022-09-06 19:29:08 +0200

    edit bindgen settings

--- build.rs
+++ build.rs
@@ -6,6 +6,8 @@ fn main() {
     println!("cargo:rerun-if-changed=cgnslib.h");
     let bindings = bindgen::Builder::default()
         .header("cgnslib.h")
+        .default_enum_style(bindgen::EnumVariation::ModuleConsts)
+        .size_t_is_usize(true)
         .generate()
         .expect("generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
